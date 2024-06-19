cbda95ec2e61f28ad4d248b2957f13e381158b1d
Alvaro Separovich <alvasepro@gmail.com>
2024-06-19 09:05:35 -0300

    LINTER ADDED

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,21 @@
-import express  from 'express'
-const app = express();
-const router = express.Router();
+import express from "express"
+const app = express()
+const router = express.Router()
 
-const port = 8080;
+const port = 8080
 
-router.use(function (req,res,next) {
-    console.log('/' + req.method);
-    next();
-  });
+router.use((req, res, next) => {
+  console.log(`/${req.method}`)
+  next()
+})
 
+router.get("/", (req, res) => {
+  res.json({ key: "value" })
+})
 
-router.get('/', function(req,res){
-    res.json({key:"value"});
-  });
+app.use("/", router)
 
-
-app.use('/', router);
-
-  app.listen(port, function () {
-    console.log('Example app listening on port 8080!')
-    console.log(process.env.PORT)
-  })
+app.listen(port, () => {
+  console.log("Example app listening on port 8080!")
+  console.log(process.env.PORT)
+})
